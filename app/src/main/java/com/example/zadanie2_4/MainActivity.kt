f3ef22b981a59519ca85709e019af790b0f9891e
Groml <gavrilova.yulia.a@yuandex.ru>
2022-12-14 02:35:27 +0300

    zadanie

---
+++ app/src/main/java/com/example/zadanie2_4/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.zadanie2_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    lateinit var firstFragment: FirstFragment
+    lateinit var secondFragment: SecondFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        firstFragment = FirstFragment.newInstance()
+        secondFragment = SecondFragment.newInstance()
+    }
+
+    fun switchToFirstFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, firstFragment)
+            .commit()
+    }
+
+    fun switchToSecondFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, secondFragment)
+            .commit()
+    }
+}
